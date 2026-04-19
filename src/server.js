d37ed7028f73bd7ae11eefc1debfea597fbe3cc9
chaeriljayaputra <chaeriljayaputra09@gmail.com>
2026-04-19 10:57:12 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ import express from "express";
 import { startBot } from "./bot.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Lonnie bot aktif 🚀");
+  res.send("Bot aktif 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log("Server jalan di port", PORT);
+  console.log("Server jalan");
   startBot();
 });
